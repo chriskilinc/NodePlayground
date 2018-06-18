2ab72cf9ce7efedb80f60dfc66821c9c8dd48beb
chriskilinc <chriskilinc@gmail.com>
2018-06-18 15:04:33 +0200

    Added an 'async' readFile function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+const fs = require('fs');
+const util = require('util');
+
+// Convert fs.readFile into Promise version 
+const readFile = util.promisify(fs.readFile);
+
+//  main()
+async function main() {
+  // Can't use `await` outside of an async function so you need to chain with then()
+  await getFile('./media/test.txt', 'utf8').then(data => {
+    console.log(data);
+  }).catch(err => {
+    console.log(err);
+  })
+  console.log("Hello World");
+}
+
+//  Will return contents of file in specific encoding
+async function getFile(path, options) {
+  return await readFile(path, options);
+}
+
+main();
+
+
